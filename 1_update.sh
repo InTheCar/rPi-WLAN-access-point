6da633e28374af9d88e372e35fd070dc8be08d7b
L@myterhell.de <L@myterhell.de>
2025-10-17 23:48:23 +0200

    in preparation

--- 1_update.sh
+++ 1_update.sh
@@ -8,10 +8,13 @@ SECONDS=0
 for i in "${command[@]}"
 do
   echo ""
-  echo "perform $i"
-  eval $1 || "echo failed: $1"
+  echo "perform: $i"
+  eval $i || echo failed: $i
+  #eval "$1"
 done
+echo ""
 duration=$SECONDS
 echo "time for updates needed:"
 echo "$((duration / 60)) minutes and $((duration % 60)) seconds elapsed."
+echo ""
 echo "reboot needed"
