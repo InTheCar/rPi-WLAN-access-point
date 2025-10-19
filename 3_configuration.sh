83545c9b143bec4e4a4609a48996048f72879824
L@myterhell.de <L@myterhell.de>
2025-10-19 17:39:26 +0200

    251019

--- 3_configuration.sh
+++ 3_configuration.sh
@@ -15,8 +15,6 @@ sudo cp ./conf/NetworkManager/* /etc/NetworkManager/
 #sudo cp ./conf/hostapd/* /etc/hostapd/
 #sudo cp /etc/hostapd/hostapd_WPA_PSK.conf /etc/hostapd/hostapd.conf
 #sudo cp ./conf/etc/* /etc/
-#sudo systemctl unmask hostapd
-#sudo systemctl enable hostapd
 #sudo systemctl restart dhcpcd
 #sudo systemctl restart dnsmasq
 #sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
@@ -31,5 +29,8 @@ sudo cp ./conf/netplan/51-configure_WPA2_PSK.yaml /etc/netplan/
 sudo chmod 600 /etc/netplan/*
 sudo netplan generate
 sudo netplan apply
+sudo systemctl unmask hostapd
+sudo systemctl enable hostapd
+sudo systemctl start hostapd
 
 
