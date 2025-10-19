fd67c1d0efeb748e6fe900dbcbdfb278abaae567
L@myterhell.de <L@myterhell.de>
2025-10-19 18:28:55 +0200

    251019

--- 3_configuration.sh
+++ 3_configuration.sh
@@ -54,11 +54,11 @@ sudo systemctl restart dnsmasq.service
 #sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 #sudo iptables-save
 
-sudo rm /etc/netplan/51-*
-sudo cp ./conf/netplan/51-configure_WPA2_PSK.yaml /etc/netplan/
-sudo chmod 600 /etc/netplan/*
-sudo netplan generate
-sudo netplan apply
+#sudo rm /etc/netplan/51-*
+#sudo cp ./conf/netplan/51-configure_WPA2_PSK.yaml /etc/netplan/
+#sudo chmod 600 /etc/netplan/*
+#sudo netplan generate
+#sudo netplan apply
 #sudo systemctl unmask hostapd
 #sudo systemctl enable hostapd
 #sudo systemctl start hostapd
