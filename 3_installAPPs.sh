08d8a9b3f2eda7d42b22986ca7b5969d4c24da0c
L@myterhell.de <L@myterhell.de>
2025-10-19 21:38:20 +0200

    251019

--- 3_installAPPs.sh
+++ 3_installAPPs.sh
@@ -1,11 +1,34 @@
 #!/bin/bash
+command=(
+  "sudo snap refresh"
+  "sudo apt-get update"
+  "sudo apt-get -y upgrade"
+)
+SECONDS=0
+for i in "${command[@]}"
+do
+  echo ""
+  echo "perform: $i"
+  eval $i || echo failed: $i
+  #eval "$1"
+done
+echo ""
+duration=$SECONDS
+echo "time for updates needed:"
+echo "$((duration / 60)) minutes and $((duration % 60)) seconds elapsed."
+echo ""
+
+
 apps=(
   "network-manager"
   "openvswitch-switch-dpdk"
   "hostapd"
-  "dnsmasq"
   "net-tools"
-  "linux-modules-extra-raspi"
+  "dnsmasq"
+  "iptables"
+  "rfkill"
+  "wireless-tools"
+
 )
 #  "dnsmasq"
 #  "hostapd"
